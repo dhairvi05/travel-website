b920898b81ba58e6e5fdbacc7c6badb1361cae59
dhairvi05 <dhairvisangani@gmail.com>
2025-06-25 17:44:57 +0530

    browser to hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router,Routes,Route,Navigate} from 'react-router-dom'
+import {HashRouter as Router,Routes,Route,Navigate} from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Places from './Components/Places/Places'
